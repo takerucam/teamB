b712bace0e561b72bb4bb93f3d4fc438d48a0ab2
gakuseidantai-with <m18kecam777@gmail.com>
2021-09-09 15:38:19 +0900

    ✨ microCMS SDKの導入、データ取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
+import { client } from 'src/libs/client'
+import { ClientResponse, Property } from 'src/types/propertys'
 
-const Index: NextPage = () => {
+type Props = {
+  propertys: Property[]
+}
+
+const Index: NextPage<Props> = ({ propertys }) => {
+  console.log(propertys)
+
   return (
-    <div className="p-4 mx-3 text-red-500">
-      これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはサンプルです。これはテストです。これはテストです。
-    </div>
+    <>
+      <div className="p-4 mx-3 text-red-500"></div>
+    </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await client.get<ClientResponse>({ endpoint: 'property' })
+  return {
+    props: {
+      propertys: data.contents,
+    },
+  }
 }
 
 export default Index
---
+++ src/types/propertys.ts
@@ -0,0 +1,35 @@
+// 画像
+export type Image = {
+  url: string
+  height: number
+  width: number
+}
+
+// タイムスタンプ群
+export type TimeStaps = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+}
+
+// 物件データ
+export type Property = TimeStaps & {
+  MonthlyAmount: string
+  AgeOfBuilding: string
+  FloorPlan: string
+  Area: string
+  Location: string
+  Latitude: string
+  Longitude: string
+  BuildingType: string
+}
+
+// microcmsからのレスポンス
+export type ClientResponse = {
+  contents: Property[]
+  totalcount: number
+  offset: number
+  limit: number
+}
