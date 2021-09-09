45b55c826471fb6ffab69d583140a9eac2a3a011
gakuseidantai-with <m18kecam777@gmail.com>
2021-09-09 19:37:29 +0900

    🐛 Google Map表示

--- src/components/GoogleMaps.tsx
+++ src/components/GoogleMaps.tsx
@@ -19,32 +19,9 @@ const center = {
 }
 
 export const GoogleMaps = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.REACT_GOOGLE_MAPS_API_KEY,
-  })
-
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds()
-    map.fitBounds(bounds)
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={17}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    ></GoogleMap>
-  ) : (
-    <></>
+  return (
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_MAP_KEY}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}></GoogleMap>
+    </LoadScript>
   )
 }
